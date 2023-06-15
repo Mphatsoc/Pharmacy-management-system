f0985f276e4b39f567f65f436340f6a93e92aaf4
Oswald <61564330+bsc-inf-10-18@users.noreply.github.com>
2023-06-15 10:42:20 +0200

    automatically deduct stock card upon approval

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -5,6 +5,10 @@ use App\Models\User;
 use App\Models\StockCard;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Models\Medicine;
+use Carbon\Carbon;
+
+
 
 class NotificationsController extends Controller
 {
@@ -25,12 +29,34 @@ class NotificationsController extends Controller
     }
     public function approved($id)
     {
-    $riv = Riv::find($id);
-    $riv->status='Approved';
-    $riv->save();
-
-    return redirect()->back();
+        $riv = Riv::find($id);
+        $riv->status = 'Approved';
+        $riv->save();
+    
+        // Subtract the medicine quantity
+        $medicine = Medicine::where('medicine_name', $riv->medicine_name)
+                            ->first();
+    
+        if ($medicine) {
+            $medicine->quantity_on_hand -= $riv->quantity_requested;
+            $medicine->save();
+    
+            // Create a new StockCard instance
+            $stockCard = new StockCard;
+            $stockCard->medicine_name = $medicine->medicine_name;
+            $stockCard->date = Carbon::now()->format('Y-m-d');
+            $stockCard->quantity_received = 0;
+            $stockCard->quantity_issued = $riv->quantity_requested;
+            $stockCard->quantity_on_hand = $medicine->quantity_on_hand;
+            $stockCard->losses = 0;
+            $stockCard->original_quantity_on_hand = $medicine->quantity_on_hand + $riv->quantity_requested;
+            $stockCard->new_quantity_on_hand = $medicine->quantity_on_hand;
+            $stockCard->save();
+        }
+    
+        return redirect()->back();
     }
+    
     public function declined($id)
     {
     $riv = Riv::find($id);
