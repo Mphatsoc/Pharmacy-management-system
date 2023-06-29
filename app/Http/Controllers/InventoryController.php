602ddf8f6e234fcb9fbf88e981f1b031a53a1514
Oswald <61564330+bsc-inf-10-18@users.noreply.github.com>
2023-06-29 15:42:02 +0200

    fixed quantity issue

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -20,41 +20,39 @@ class InventoryController extends Controller
     function addData(Request $request, $id)
     {
         $medicine = Medicine::findOrFail($id);
-    
-        // Retrieve input values from the request
-        $quantityReceived = $request->input('quantity_received');
-        $losses = $request->input('losses');
-        $quantityIssued = $request->input('quantity_issued');
-        $originalQuantityOnHand = $request->input('quantity_on_hand');
 
-    
-        $loss = $losses + $quantityIssued;
-    
-        // Calculate the new quantity on hand
-        $newQuantityOnHand = $originalQuantityOnHand + $quantityReceived - ($loss + $quantityIssued);
+// Retrieve input values from the request
+$quantityReceived = $request->input('quantity_received');
+$losses = $request->input('losses');
+$quantityIssued = $request->input('quantity_issued');
+$originalQuantityOnHand = $medicine->quantity_on_hand;
 
-         // Ensure the quantity on hand doesn't go below zero
-        $newQuantityOnHand = max(0, $newQuantityOnHand);
-    
-        // Update the medicine's quantity on hand
-        $medicine->quantity_on_hand = $newQuantityOnHand;
-        $medicine->save();
-    
-        // Create a new StockCard instance
-        $stockCard = new StockCard;
-        $stockCard->medicine_name = $medicine->medicine_name;
-        $stockCard->date = Carbon::now()->format('Y-m-d');
-        $stockCard->quantity_received = $quantityReceived;
-        $stockCard->quantity_issued = $quantityIssued;
-        $stockCard->quantity_on_hand = $newQuantityOnHand;
-        $stockCard->losses = $losses;
-        $stockCard->original_quantity_on_hand = $originalQuantityOnHand;
-        $stockCard->new_quantity_on_hand = $newQuantityOnHand;
-        $stockCard->save();
-    
-        Session::flash('success', 'Stock Updated Successfully!');
-    
-        return redirect()->back()->with('success', 'Stock Updated Successfully');
+// Calculate the new quantity on hand
+$newQuantityOnHand = $originalQuantityOnHand + $quantityReceived - ($quantityIssued + $losses);
+
+// Ensure the quantity on hand doesn't go below zero
+$newQuantityOnHand = max(0, $newQuantityOnHand);
+
+// Update the medicine's quantity on hand
+$medicine->quantity_on_hand = $newQuantityOnHand;
+$medicine->save();
+
+// Create a new StockCard instance
+$stockCard = new StockCard;
+$stockCard->medicine_name = $medicine->medicine_name;
+$stockCard->date = Carbon::now()->format('Y-m-d');
+$stockCard->quantity_received = $quantityReceived;
+$stockCard->quantity_issued = $quantityIssued;
+$stockCard->quantity_on_hand = $newQuantityOnHand;
+$stockCard->losses = $losses;
+$stockCard->original_quantity_on_hand = $originalQuantityOnHand;
+$stockCard->new_quantity_on_hand = $newQuantityOnHand;
+$stockCard->save();
+
+Session::flash('success', 'Stock Updated Successfully!');
+
+return redirect()->back()->with('success', 'Stock Updated Successfully');
+
     }
     
     //This method is used to display the quantity of a specific medicine.
