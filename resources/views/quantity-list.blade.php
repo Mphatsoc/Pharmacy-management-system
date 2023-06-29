d6593ccd5add77f84d9f8c0f5ada5fa02dd6123c
HazelAshley <bsc-inf-11-18@unima.ac.mw>
2023-06-30 00:20:13 +0200

    update quantity-list blade

--- resources/views/quantity-list.blade.php
+++ resources/views/quantity-list.blade.php
@@ -296,11 +296,11 @@
             <!-- Place this tag in your head or just before your close body tag. -->
             <script async defer src="https://buttons.github.io/buttons.js"></script>
 
-<script>
+<!-- <script>
     var stockOnHand = 10;
     if (stockOnHand == 10){
-    alert('stock is low');
-}
+    alert('stock is low'); -->
+
 </script>      
     
 </body>
